ecfa9e83ff36e5cd1ebaacc796c180be6784bb98
Remi KEAT <rkeat@users.noreply.github.com>
2017-04-09 20:09:10 +0900

    Add support for more than 1 sensor

--- build_database.sh
+++ build_database.sh
@@ -3,6 +3,6 @@
 # build_database.sh - create empty temperature database schema for to log temperature in.
 #
 # Tom Holderness 22/01/2012
-sqlite3 piTemps.db 'DROP TABLE temperature_records;'
-sqlite3 piTemps.db 'CREATE TABLE temperature_records(unix_time bigint primary key, celsius real);' 
+sqlite3 piTemps3.db 'DROP TABLE temperature_records;'
+sqlite3 piTemps3.db 'CREATE TABLE temperature_records(unix_time bigint primary key, Fermenter real, Chamber real, Room real);' 
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js - NodeJS server for the PiThermServer project.
 
-/* 
+/*
 
 Parses data from DS18B20 temperature sensor and serves as a JSON object.
 Uses node-static module to serve a plot of current temperature (uses highcharts).
@@ -11,6 +11,7 @@ Ref: www.cl.cam.ac.uk/freshers/raspberrypi/tutorials/temperature/
 
 // Load node modules
 var fs = require('fs');
+var async = require('async');
 var sys = require('sys');
 var http = require('http');
 var sqlite3 = require('sqlite3');
@@ -22,50 +23,88 @@ var nodestatic = require('node-static');
 var staticServer = new nodestatic.Server(".");
 
 // Setup database connection for logging
-var db = new sqlite3.Database('./piTemps.db');
+var db = new sqlite3.Database('./piTemps3.db');
+
+var files = [
+'/sys/bus/w1/devices/28-031674c7f4ff/w1_slave',
+'/sys/bus/w1/devices/28-05167357f6ff/w1_slave',
+'/sys/bus/w1/devices/28-0516736063ff/w1_slave'
+];
+var files = [
+'temp1.txt',
+'temp2.txt',
+'temp3.txt'];
+var fields = [
+'Fermenter',
+'Chamber',
+'Room'];
+
+var msecs = 1000;//(60 * 2) * 1000; // log interval duration in milliseconds
 
 // Write a single temperature record in JSON format to database table.
-function insertTemp(data){
-   // data is a javascript object   
-   var statement = db.prepare("INSERT INTO temperature_records VALUES (?, ?)");
-   // Insert values into prepared statement
-   statement.run(data.temperature_record[0].unix_time, data.temperature_record[0].celsius);
-   // Execute the statement
-   statement.finalize();
+function insertTemp(data)
+{
+    // data is a javascript object
+    var sql_command = "INSERT INTO temperature_records VALUES (?, ";
+    var command = 'statement.run(data.temperature_record[0].unix_time, ';
+    for(var i = 0, l = fields.length-1; i < l; i++)
+    {
+        sql_command += "?, ";
+        command += 'data.temperature_record[0].'+fields[i]+', ';
+    }
+    sql_command += "?)";
+    command += 'data.temperature_record[0].'+fields[l]+');';
+    var statement = db.prepare(sql_command);
+    
+    // Insert values into prepared statement
+    eval(command);
+    // Execute the statement
+    statement.finalize();
+}
+
+function tempHandler(callback)
+{
+    return function (err, buffer)
+    {
+		    var temp_record = {
+			      temperature_record:[{
+			      unix_time: Date.now()
+		    }]};
+		    for(var i = 0, l = buffer.length; i < l; i++)
+		    {
+		        if (err)
+		        {
+	              console.error(err);
+	              process.exit(1);
+		        }
+
+		        // Read data from file (using fast node ASCII encoding).
+		        var data = buffer[i].toString('ascii').split(" "); // Split by space
+
+		        // Extract temperature from string and divide by 1000 to give celsius
+		        var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+
+		        // Round to one decimal place
+		        temp = Math.round(temp * 10) / 10;
+
+		        // Add date/time to temperature
+		        eval('temp_record.temperature_record[0].'+fields[i]+' = temp');
+		    }
+		
+		    // Execute call back with data
+		    callback(temp_record);
+    }
 }
 
 // Read current temperature from sensor
-function readTemp(callback){
-   fs.readFile('/sys/bus/w1/devices/28-00000400a88a/w1_slave', function(err, buffer)
-	{
-      if (err){
-         console.error(err);
-         process.exit(1);
-      }
-
-      // Read data from file (using fast node ASCII encoding).
-      var data = buffer.toString('ascii').split(" "); // Split by space
-
-      // Extract temperature from string and divide by 1000 to give celsius
-      var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
-
-      // Round to one decimal place
-      temp = Math.round(temp * 10) / 10;
-
-      // Add date/time to temperature
-   	var data = {
-            temperature_record:[{
-            unix_time: Date.now(),
-            celsius: temp
-            }]};
-
-      // Execute call back with data
-      callback(data);
-   });
+function readTemp(callback)
+{		  
+    async.map(files, fs.readFile, tempHandler(callback));
 };
 
 // Create a wrapper function which we'll use specifically for logging
-function logTemp(interval){
+function logTemp(interval)
+{
       // Call the readTemp function with the insertTemp function as output to get initial reading
       readTemp(insertTemp);
       // Set the repeat interval (milliseconds). Third argument is passed as callback function to first (i.e. readTemp(insertTemp)).
@@ -73,21 +112,24 @@ function logTemp(interval){
 };
 
 // Get temperature records from database
-function selectTemp(num_records, start_date, callback){
-   // - Num records is an SQL filter from latest record back trough time series, 
-   // - start_date is the first date in the time-series required, 
-   // - callback is the output function
-   var current_temp = db.all("SELECT * FROM (SELECT * FROM temperature_records WHERE unix_time > (strftime('%s',?)*1000) ORDER BY unix_time DESC LIMIT ?) ORDER BY unix_time;", start_date, num_records,
-      function(err, rows){
-         if (err){
-			   response.writeHead(500, { "Content-type": "text/html" });
-			   response.end(err + "\n");
-			   console.log('Error serving querying database. ' + err);
-			   return;
-				      }
-         data = {temperature_record:[rows]}
-         callback(data);
-   });
+function selectTemp(num_records, start_date, callback)
+{
+    // - Num records is an SQL filter from latest record back trough time series,
+    // - start_date is the first date in the time-series required,
+    // - callback is the output function
+    var current_temp = db.all("SELECT * FROM (SELECT * FROM temperature_records WHERE unix_time > (strftime('%s',?)*1000) ORDER BY unix_time DESC LIMIT ?) ORDER BY unix_time;", start_date, num_records,
+        function(err, rows)
+        {
+            if (err)
+            {
+                response.writeHead(500, { "Content-type": "text/html" });
+                response.end(err + "\n");
+                console.log('Error serving querying database. ' + err);
+                return;
+            }
+            data = {temperature_record:[rows]}
+            callback(data);
+        });
 };
 
 // Setup node http server
@@ -98,7 +140,7 @@ var server = http.createServer(
 		// Grab the URL requested by the client and parse any query options
 		var url = require('url').parse(request.url, true);
 		var pathfile = url.pathname;
-      var query = url.query;
+    var query = url.query;
 
 		// Test to see if it's a database query
 		if (pathfile == '/temperature_query.json'){
@@ -115,26 +157,31 @@ var server = http.createServer(
          }
          else{
             var start_date = '1970-01-01T00:00';
-         }   
+         }
          // Send a message to console log
          console.log('Database query request from '+ request.connection.remoteAddress +' for ' + num_obs + ' records from ' + start_date+'.');
          // call selectTemp function to get data from database
          selectTemp(num_obs, start_date, function(data){
-            response.writeHead(200, { "Content-type": "application/json" });		
+            response.writeHead(200, { "Content-type": "application/json" });
 	         response.end(JSON.stringify(data), "ascii");
          });
       return;
       }
-      
-      // Test to see if it's a request for current temperature   
+
+      // Test to see if it's a request for current temperature
       if (pathfile == '/temperature_now.json'){
             readTemp(function(data){
-			      response.writeHead(200, { "Content-type": "application/json" });		
+			      response.writeHead(200, { "Content-type": "application/json" });
 			      response.end(JSON.stringify(data), "ascii");
                });
       return;
       }
       
+    if (pathfile == '/')
+    {
+      pathfile = '/index.htm';
+    }
+
       // Handler for favicon.ico requests
 		if (pathfile == '/favicon.ico'){
 			response.writeHead(200, {'Content-Type': 'image/x-icon'});
@@ -145,33 +192,31 @@ var server = http.createServer(
 			return;
 		}
 
-
 		else {
 			// Print requested file to terminal
 			console.log('Request from '+ request.connection.remoteAddress +' for: ' + pathfile);
-
-			// Serve file using node-static			
+      request.url = pathfile;
+			// Serve file using node-static
 			staticServer.serve(request, response, function (err, result) {
 					if (err){
 						// Log the error
 						sys.error("Error serving " + request.url + " - " + err.message);
-						
+
 						// Respond to the client
 						response.writeHead(err.status, err.headers);
 						response.end('Error 404 - file not found');
 						return;
 						}
-					return;	
+					return;
 					})
 		}
 });
 
-// Start temperature logging (every 5 min).
-var msecs = (60 * 5) * 1000; // log interval duration in milliseconds
+// Start temperature logging.
 logTemp(msecs);
 // Send a message to console
 console.log('Server is logging to database at '+msecs+'ms intervals');
 // Enable server
-server.listen(8000);
+server.listen(7000);
 // Log message
-console.log('Server running at http://localhost:8000');
+console.log('Server running at http://localhost:7000');
